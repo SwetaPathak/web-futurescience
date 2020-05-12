3dd27adaf701815c8e3a70c06d3e3b596335b7a7
Jithin Joji Anchanattu <jithin.anchanattu@gmail.com>
2020-05-13 02:06:52 +0530

    Commit

--- js/main.js
+++ js/main.js
@@ -546,7 +546,7 @@
             var mapOptions = {
                 zoom: 15,
                 scrollwheel: false,
-                center: new google.maps.LatLng(-37.81618, 144.95692)
+                center: new google.maps.LatLng(9.9973708,76.3608823) 
             };
             var map = new google.maps.Map(document.getElementById("googleMap"),
                 mapOptions);
